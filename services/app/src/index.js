d2800b0a2ffdc4733137662314ea8cd86382e659
Anton Shvab <shwab.ay@gmail.com>
2018-09-01 11:16:44 +0300

    wip

--- services/app/src/index.js
+++ services/app/src/index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
+const serve = require('koa-static');
+
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+app.use(serve('static'));
 
-console.log("!!!");
+// app.use(async ctx => {
+//   ctx.body = 'Hello World';
+// });
 
 app.listen(3000);
