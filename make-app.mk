7cc4c379028c7dd4bafeca4a0b2445901e11b306
Anton Shvab <shwab.ay@gmail.com>
2018-09-02 07:53:19 +0300

    Checkpoint 1.
    Make development environment with docker-compose

--- make-app.mk
+++ make-app.mk
@@ -1,19 +1,22 @@
 USER = "$(shell id -u):$(shell id -g)"
 
+# TODO
+# make npm can install without root rights
+
 app:
 	docker-compose up
 
 app-build:
 	docker-compose build
 
-# app-bash:
-# 	docker-compose run --user=$(USER) app bash
-
 app-bash:
+	docker-compose run --user=$(USER) app bash
+
+app-bash-root:
 	docker-compose run app bash
 
-# app-setup: app-build
-# 	docker-compose run --user=$(USER) app npm install
-
 app-setup: app-build
+	docker-compose run --user=$(USER) app npm install
+
+app-setup-root: app-build
 	docker-compose run app npm install
